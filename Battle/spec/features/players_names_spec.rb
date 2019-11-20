c9e78ae6e9e248774cd1011d016c4798a02615dd
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-20 14:54:59 +0000

    battle added sessions and added some text colouring

--- Battle/spec/features/players_names_spec.rb
+++ Battle/spec/features/players_names_spec.rb
@@ -4,8 +4,6 @@ feature 'Testing input and redirect' do
     fill_in "Player 1", :with => "Jo"
     fill_in "Player 2", :with => "Matt"
     click_button "Submit"
-    expect(page).to have_content 'Player 1 is Jo'
-    expect(page).to have_content 'Player 2 is Matt'
     expect(page).to have_content 'Jo vs Matt'
   end
 end
