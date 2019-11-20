59149d28cb4637090f338be54d933f9674f92155
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-20 17:07:56 +0000

    failed feature test for attack which reduces the HP

--- Battle/spec/features/attack_spec.rb
+++ Battle/spec/features/attack_spec.rb
@@ -1,7 +1,10 @@
 feature 'Attacking' do
-  scenario 'attack Player 2' do
+
+  scenario 'attack Player 2 and reduce HP by 10' do
     sign_in_and_play
-    click_link 'Attack'
-    expect(page).to have_content 'Jo attacked Matt'
+    click_button 'Attack'
+    click_link 'Ok'
+    expect(page).not_to have_content 'Matt: 50HP'
+    expect(page).to have_content 'Matt: 40HP'
   end
 end
