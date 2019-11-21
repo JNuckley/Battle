844e004636b871081ce55409f9f208c08d7c7da4
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-21 11:07:31 +0000

    Comments added

--- Battle/app.rb
+++ Battle/app.rb
@@ -4,30 +4,30 @@ require './lib/player'
 
 class Battle < Sinatra::Base
 enable :sessions
-
+#puts the form on the home screen and title using the erb index file
 get '/' do
   erb :index
 end
-
+#assigns the global variable of $p1 to p1_name to store
 post '/names' do
   $p1 = Player.new(params[:p1_name])
   $p2 = Player.new(params[:p2_name])
   redirect :play
 end
-
+#assigning p2.hit_points to the instance variable for later use
 get '/play' do
-  @p1_name = $p1.name
-  @p2_name = $p2.name
+  # @p1_name = $p1.name
+  # @p2_name = $p2.name
   @p2_hit_points = $p2.hit_points
   erb :play
 end
-
+#calling the attack method on the global variable for p1 to attack p2
 get '/attack' do
-  @p1_name = $p1.name
-  @p2_name = $p2.name
+  # @p1_name = $p1.name
+  # @p2_name = $p2.name
   $p1.attack($p2)
   erb :attack
 end
-
+#this allows programme to be run by just running the app file
 run! if app_file == $0
 end
